730a97519a82315bfa4af5bb2d756e5bfc6a383f
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-03-12 23:48:03 +0200

    change newStatus var name to newSaveStatus in order to make it more readable Relates #5

--- src/database/queries/unsaveResavePostQuery.js
+++ src/database/queries/unsaveResavePostQuery.js
@@ -1,5 +1,5 @@
 const { connection } = require('../config');
 
-const unsaveResavePostQuery = ({ newStatus, saveId }) => connection.query('UPDATE saved_posts SET is_post_saved=$1 WHERE id=$2', [newStatus, saveId]);
+const unsaveResavePostQuery = ({ newSaveStatus, saveId }) => connection.query('UPDATE saved_posts SET is_post_saved=$1 WHERE id=$2', [newSaveStatus, saveId]);
 
 module.exports = { unsaveResavePostQuery };
