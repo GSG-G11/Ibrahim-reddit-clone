6dd911983b71d652b5c7e8265bf30b4e26b4d4d2
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-03-14 03:21:07 +0200

    addComment controller relates #6

--- src/controllers/addComment.js
+++ src/controllers/addComment.js
@@ -1,5 +1,13 @@
-const addComment = () => {
+const { addCommentQuery } = require('../database/queries');
+const { postIdValidation, newCommentValidation } = require('../utils/validation');
 
+const addComment = (req, res) => {
+  const userId = req.user.id;
+  const { postId } = req.params;
+  const { comment } = req.body;
+  Promise.all([postIdValidation(req.params), newCommentValidation(req.body)])
+    .then(() => addCommentQuery({ comment, userId, postId }))
+    .then(() => res.status(201).json({ status: 201, message: 'comment added' }))
+    .catch((err) => res.json(err));
 };
-
 module.exports = { addComment };
--- src/utils/validation/newCommentValidation.js
+++ src/utils/validation/newCommentValidation.js
@@ -2,8 +2,6 @@ const Joi = require('joi');
 
 const commentSchema = Joi.object({
   comment: Joi.string().required(),
-  postId: Joi.number().required(),
-  commentId: Joi.number().required(),
 });
 
 const newCommentValidation = (dataObj) => commentSchema.validateAsync(dataObj);
