d3d340e8e390485264f4ffac15de4fc2565156a9
Ibrahim-Jarada <ibrahim.jarada@gmail.com>
2022-03-16 09:06:31 +0200

    update signup schema Relates #6

--- src/utils/validation/signupValidation.js
+++ src/utils/validation/signupValidation.js
@@ -1,9 +1,9 @@
 const Joi = require('joi');
 
 const signupSchema = Joi.object({
-  username: Joi.string().min(3).required(),
+  username: Joi.string().pattern(/^[a-zA-Z0-9_-]{3,20}$/).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().pattern(/^[a-zA-Z0-9]{6,30}$/).required(),
+  password: Joi.string().pattern(/^[a-zA-Z0-9]{8,30}$/).required(),
 });
 
 const signupValidation = (dataObj) => signupSchema.validateAsync(dataObj);
